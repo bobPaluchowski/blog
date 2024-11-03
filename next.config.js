f196ae3b2c7c16d278fa424fb68628690fb07904
sys-77 <systemseventy7@gmail.com>
2024-11-03 22:38:02 +0100

    deploy blog...

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   basePath:isProd ? '/my_blog' : '',
   assetPrefix: isProd ? '/my_blog' : '',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
