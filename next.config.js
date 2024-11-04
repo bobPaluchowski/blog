a245581dcd9e82d4d4516de26583189b3f843fef
sys-77 <systemseventy7@gmail.com>
2024-11-04 06:56:30 +0100

    path is needed in next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  // basePath:isProd ? '/my_blog' : '',
-  // assetPrefix: isProd ? '/my_blog' : '',
+  basePath:isProd ? '/my_blog' : '',
+  assetPrefix: isProd ? '/my_blog' : '',
   output: 'export',
   // images: {
     // unoptimized: true,
